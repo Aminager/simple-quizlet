4e49f2dbd1a960a56b98969377375bcb9ab45207
Amin Alian <alianamin00@gmail.com>
2023-10-16 14:01:27 +0200

    Added GPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Card } from "./Card";
 import questionList from "../questiondkse.json";
+import { GPT } from "./GPT";
 
 function App() {
   const [lang, setLang] = useState(true);
@@ -18,6 +19,7 @@ function App() {
 
   const handleNext = () => {
     const next = Math.floor(Math.random() * questionList["questions"].length);
+    console.log(next);
     setQuestion(
       lang ? questionList["questions"][next] : questionList["answers"][next]
     );
@@ -29,23 +31,31 @@ function App() {
     );
   };
   return (
-    <div className="flex flex-col h-screen justify-center items-center gap-4">
-      <span>
-        Current language: {lang ? "Dansk -> svenska" : "Svenska -> dansk"}
-      </span>
-      <button
-        onClick={switchLang}
-        className="rounded-lg border-black border-2 w-1/12"
-      >
-        Switch language
-      </button>
-      <Card question={question} answer={answer} text={text} setText={setText} />
-      <button
-        onClick={handleNext}
-        className="rounded-lg border-black border-2 w-1/12"
-      >
-        Next
-      </button>
+    <div className="flex flex-row h-screen justify-center items-center gap-4">
+      {lang ? <div /> : <GPT word={question} />}
+      <div className="flex flex-col h-full w-full justify-center items-center gap-4">
+        <span>
+          Current language: {lang ? "Dansk -> svenska" : "Svenska -> dansk"}
+        </span>
+        <button
+          onClick={switchLang}
+          className="rounded-lg border-black border-2"
+        >
+          Switch language
+        </button>
+        <Card
+          question={question}
+          answer={answer}
+          text={text}
+          setText={setText}
+        />
+        <button
+          onClick={handleNext}
+          className="rounded-lg border-black border-2 w-1/12"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/GPT.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const API_KEY = import.meta.env.VITE_GPT_KEY;
+
+export const GPT = (word) => {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm ChatGPT! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSendRequest = async (message) => {
+    const newMessage = {
+      message: `Generera en dansk mening med understreck där man kan fylla i istället för det svenska ordet ${word["word"]}`,
+      direction: "outgoing",
+      sender: "user",
+    };
+    const placeholderMessage = {
+      message: `Genererar en dansk mening`,
+      direction: "outgoing",
+      sender: "user",
+    };
+
+    setMessages((prevMessages) => [placeholderMessage]);
+    setIsTyping(true);
+
+    try {
+      const response = await processMessageToChatGPT([newMessage]);
+      const content = response.choices[0]?.message?.content;
+      if (content) {
+        const chatGPTResponse = {
+          message: content,
+          sender: "ChatGPT",
+        };
+        setMessages((prevMessages) => [chatGPTResponse]);
+      }
+    } catch (error) {
+      console.error("Error processing message:", error);
+    } finally {
+      setIsTyping(false);
+    }
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === "ChatGPT" ? "assistant" : "user";
+      return { role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "I'm a Student using ChatGPT for learning" },
+        ...apiMessages,
+      ],
+    };
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    });
+
+    return response.json();
+  }
+  return (
+    <div className="flex flex-col max-w-4xl max-h-md w-full justify-center items-center">
+      <div className="w-full">
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                isTyping ? (
+                  <TypingIndicator content="ChatGPT is typing" />
+                ) : null
+              }
+            >
+              {messages.map((message, i) => {
+                console.log(message);
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+          </ChatContainer>
+        </MainContainer>
+      </div>
+      <button
+        className="rounded-md border-2 border-black place-self-center"
+        onClick={handleSendRequest}
+      >
+        Generate sentence
+      </button>
+    </div>
+  );
+};
